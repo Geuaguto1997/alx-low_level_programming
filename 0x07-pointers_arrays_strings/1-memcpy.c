d55b47d0c39560719f01f975edc742b43fceeb9a
Geuaguto1997 <geuaguto2019@gmail.com>
2023-04-04 13:46:37 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,11 +8,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-        unsigned int i;
-
-        for (i = 0; i <= n - 1; i++)
-        {
-                dest[i] = src[i];
-        }
-        return (dest);
+	unsigned int i;
+	
+	for (i = 0; i <= n - 1; i++)
+	{
+	dest[i] = src[i];
+	}
+	return (dest);
 }
