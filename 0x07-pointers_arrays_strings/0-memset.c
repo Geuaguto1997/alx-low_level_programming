b56f94319764c67c2f48b07858275d14c06e9fc7
Geuaguto1997 <geuaguto2019@gmail.com>
2023-04-04 13:30:31 -0700

    firtst task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,11 +8,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-        unsigned int i = 0;
+	unsigned int i = 0;
 
-        for (i = 0; i <= n - 1; i++)
-        {
-                s[i] = b;
-        }
-        return (s);
+	for (i = 0; i <= n - 1; i++)
+	{
+	s[i] = b;
+	}
+	return (s);
 }
